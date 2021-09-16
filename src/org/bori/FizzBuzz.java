ddeb9cdbc4d1fec022a6206a3ca65f43a065b980
Borbala Tobias <borbala.tobias@aexp.com>
2021-09-16 15:42:11 +0100

    allow user input and list and 17 changes the order

--- src/org/bori/FizzBuzz.java
+++ src/org/bori/FizzBuzz.java
@@ -1,31 +1,49 @@
 package org.bori;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
 
-        for (int i = 1; i <= 100; i++) {
-            String printable = "";
+        Scanner newScan = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int numByUser = newScan.nextInt();
+
+        for (int i = 1; i <= numByUser; i++) {
             String fezz = "Fezz";
             String fizz = "Fizz";
             String buzz = "Buzz";
             String bang = "Bang";
+            String bong = "Bong";
+
+            List<String> rules = new ArrayList<String>();
+
             if (i % 3 == 0) {
-                printable += fizz;
+                rules.add(fizz);
             }
             if (i % 13 == 0) {
-                printable += fezz;
+                rules.add(fezz);
             }
             if (i % 5 == 0) {
-                printable += buzz;
+                rules.add(buzz);
             }
             if (i % 7 == 0) {
-                printable += bang;
+                rules.add(bang);
             }
-            if (i % 11 == 0) {
-                printable = "Bong";
+            if (i % 11 == 0 && i % 13 == 0) {
+                rules.add(bong);
+            } else if (i % 11 == 0) {
+                rules = new ArrayList<String>();
+                rules.add(bong);
+            }
+            if (i % 17 == 0) {
+                Collections.reverse(rules);
             }
 
-            System.out.println(printable.isEmpty() ? i : printable);
+            System.out.println(rules.isEmpty() ? i : String.join("", rules));
 
         }
 
