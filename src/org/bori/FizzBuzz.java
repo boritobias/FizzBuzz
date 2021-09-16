b2ec136d51c09f6ffc5813840e680af0924b2de0
Borbala Tobias <borbala.tobias@aexp.com>
2021-09-16 14:38:09 +0100

    add Bang, Fezz and Bong

--- src/org/bori/FizzBuzz.java
+++ src/org/bori/FizzBuzz.java
@@ -5,13 +5,24 @@ public class FizzBuzz {
 
         for (int i = 1; i <= 100; i++) {
             String printable = "";
+            String fezz = "Fezz";
             String fizz = "Fizz";
             String buzz = "Buzz";
+            String bang = "Bang";
             if (i % 3 == 0) {
                 printable += fizz;
             }
+            if (i % 13 == 0) {
+                printable += fezz;
+            }
             if (i % 5 == 0) {
                 printable += buzz;
+            }
+            if (i % 7 == 0) {
+                printable += bang;
+            }
+            if (i % 11 == 0) {
+                printable = "Bong";
             }
 
             System.out.println(printable.isEmpty() ? i : printable);
